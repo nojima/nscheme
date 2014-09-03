16443ab8276e8dd02a6cfb8d05490aa944996655
Yusuke Nojima <nojima@ynojima.com>
2014-09-03 18:13:08 +0900

    Remove definition of embedded classes

--- src/embeded_object.hpp
+++ src/embeded_object.hpp
@@ -15,23 +15,24 @@ enum class ValueTag: std::uint64_t {
     kFalse = 4,
 };
 
-class NilObject: public Object {};
 inline bool IsNil(ObjectRef obj) {
     return obj == static_cast<ObjectRef>(ValueTag::kNil);
 }
+
 inline ObjectRef GetNilObject() {
     return static_cast<ObjectRef>(ValueTag::kNil);
 }
 
-class BooleanObject: public Object {};
 inline bool IsBoolean(ObjectRef obj) {
     return obj == static_cast<ObjectRef>(ValueTag::kTrue) ||
            obj == static_cast<ObjectRef>(ValueTag::kFalse);
 }
+
 inline bool GetBooleanValue(ObjectRef obj) {
     assert(IsBoolean(obj));
     return obj != static_cast<ObjectRef>(ValueTag::kFalse);
 }
+
 inline ObjectRef GetBooleanObject(bool b) {
     if (b)
         return static_cast<ObjectRef>(ValueTag::kTrue);
@@ -39,31 +40,32 @@ inline ObjectRef GetBooleanObject(bool b) {
         return static_cast<ObjectRef>(ValueTag::kFalse);
 }
 
-class CharacterObject: public Object {};
 inline bool IsCharacter(ObjectRef obj) {
     return (obj & 3) == static_cast<ObjectRef>(ValueTag::kCharacter);
 }
+
 inline char GetCharacterValue(ObjectRef obj) {
     assert(IsCharacter(obj));
     return static_cast<char>(obj >> 2);
 }
+
 inline ObjectRef GetCharacterObject(char ch) {
     return (ch << 2) | static_cast<ObjectRef>(ValueTag::kCharacter);
 }
 
-class IntegerObject: public Object {};
 inline bool IsInteger(ObjectRef obj) {
     return (obj & 3) == static_cast<ObjectRef>(ValueTag::kInteger);
 }
+
 inline std::int64_t GetIntegerValue(ObjectRef obj) {
     assert(IsInteger(obj));
     return static_cast<std::int64_t>(obj) >> 2;
 }
+
 inline ObjectRef GetIntegerObject(std::int64_t n) {
     return (n << 2) | static_cast<ObjectRef>(ValueTag::kInteger);
 }
 
-class SymbolObject: public Object {};
 inline bool IsSymbol(ObjectRef obj) {
     return (obj & 3) == static_cast<ObjectRef>(ValueTag::kSymbol);
 }
@@ -72,6 +74,7 @@ inline Symbol GetSymbolValue(ObjectRef obj) {
     assert(IsSymbol(obj));
     return Symbol(reinterpret_cast<const std::string*>(obj & ~static_cast<ObjectRef>(3)));
 }
+
 inline ObjectRef GetSymbolObject(Symbol symbol) {
     return static_cast<ObjectRef>(symbol.InternalId() | static_cast<intptr_t>(ValueTag::kSymbol));
 }
